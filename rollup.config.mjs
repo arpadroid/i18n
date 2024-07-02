9debe834ee65408fae48301a64a10c32cb9b832d
Andres Vaquero <web.andresvaquero@gmail.com>
2024-07-03 01:03:24 +1000

    abstracting configurations into @arpadroid/arpadroid

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
-import copy from 'rollup-plugin-copy';
-import json from '@rollup/plugin-json';
+import copy from './node_modules/@arpadroid/arpadroid/node_modules/rollup-plugin-copy/dist/index.commonjs.js';
+import json from './node_modules/@arpadroid/arpadroid/node_modules/@rollup/plugin-json/dist/cjs/index.js';
 import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
+
 const { build, plugins, appBuild } = getBuild('i18n', 'uiComponent');
 appBuild.plugins = [json(), ...plugins, copy({ targets: [{ src: 'src/lang', dest: 'dist' }] })];
 export default build;

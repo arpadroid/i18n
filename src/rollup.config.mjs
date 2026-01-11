d7e6c1990be653d7a9e5c2e6750ee4369aa319ac
Andres Vaquero <web.andresvaquero@gmail.com>
2026-01-12 00:43:32 +1100

    fix type bug in build

--- src/rollup.config.mjs
+++ src/rollup.config.mjs
@@ -1,7 +1,7 @@
 import { getBuild } from '@arpadroid/module';
-const { build = {}, plugins, appBuild, Plugins } = getBuild('i18n');
+const { build = {}, plugins, appBuild = {}, Plugins } = getBuild('i18n');
 appBuild.plugins = [
     ...(Array.isArray(plugins) ? plugins : []),
-    Plugins.copy({ targets: [{ src: 'src/lang', dest: 'dist' }] })
+    Plugins?.copy({ targets: [{ src: 'src/lang', dest: 'dist' }] })
 ];
 export default build;

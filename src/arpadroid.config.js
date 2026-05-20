6a9e4a6ac5578a4b7d2014c6cc7408b3f3daf661
Andres Vaquero <web.andresvaquero@gmail.com>
2026-05-20 23:08:01 +1000

    BAU

--- src/arpadroid.config.js
+++ src/arpadroid.config.js
@@ -3,11 +3,7 @@ const config = {
     buildTypes: true,
     buildType: 'uiComponent',
     buildStyles: false,
-    storybook_port: 6004,
-    logo: `           ┓    • ┓  •┓┏┓  
-  ┏┓┏┓┏┓┏┓┏┫┏┓┏┓┓┏┫  ┓┃┣┫┏┓
-  ┗┻┛ ┣┛┗┻┗┻┛ ┗┛┗┗┻  ┗┻┗┛┛┗
-------┛-----------------------`
+    storybook_port: 6004
 };
 
 export default config;
